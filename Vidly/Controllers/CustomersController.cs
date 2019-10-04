43d2f0bd19954c92c16e045886af03e11b0ee4c4
Lishjones <49446723+Lishjones@users.noreply.github.com>
2019-10-04 17:41:56 +0100

    Section 2 solution

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        //Customers/Details/Id
+        public ActionResult Details(int Id)
+        {
+            var customers = GetCustomers();
+            var customer = customers.SingleOrDefault<Customer>(x => x.Id == Id);
+            
+            if (customer == null)
+                return new HttpNotFoundResult();
+
+            return View(customer);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Name = "John Doe", Id = 0},
+                new Customer {Name = "Jane Doe", Id = 1}
+            };
+        }
+    }
+}
